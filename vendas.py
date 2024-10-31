20e33ee72a4649a20dba30164e39ac283529d4f9
Bruna Grassi <101432594+BrunaGrassi@users.noreply.github.com>
2024-10-31 18:00:23 -0300

    Lendo arquivo CSV  - local
    - importando localmente
    - alterando o cabeçalho

--- vendas.py
+++ vendas.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-url = "https://people.sc.fsu.edu/~jburkardt/data/csv/airtravel.csv"
+#url = "https://people.sc.fsu.edu/~jburkardt/data/csv/airtravel.csv"
+#novo_cabecalho = ["Mês","Ano 1958","Ano 1959","Ano 1960"]
 
-novo_cabecalho = ["Mês","Ano 1958","Ano 1959","Ano 1960"]
+#df = pd.read_csv(url, names=novo_cabecalho, header=0)
+#print(df.head())
 
-df = pd.read_csv(url, names=novo_cabecalho, header=0)
+novo_cabecalho2 = ["REF","PRODUTO","QTD","PREÇO","DATA"]
+
+df = pd.read_csv(r"C:\Users\brude\OneDrive\Área de Trabalho\Projetos Pessoais Programação\Python\vendas.csv", names=novo_cabecalho2, header=0)
 print(df.head())
